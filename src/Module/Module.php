4d4cc1dd9038a9fd6b9788bc2af2f273e7734f13
Christian <cmb@schumacher-visuell.de>
2022-02-09 13:52:41 +0100

    beforeGetJson array $payload
    PHP Warning: Declaration of Theme\Module\ContactformModule::beforeGetJSON($payload) should be compatible with Sloth\Module\Module::beforeGetJSON() in .../themes/smr/Module/ContactformModule.php:27

--- src/Module/Module.php
+++ src/Module/Module.php
@@ -35,7 +35,7 @@ class Module
 
     final public function getAjaxAction()
     {
-        return 'module_' . Utility::underscore(class_basename($this));
+        return 'module_'.Utility::underscore(class_basename($this));
     }
 
     final public function getAjaxUrl()
@@ -43,7 +43,7 @@ class Module
         return str_replace(
             home_url(),
             '',
-            \admin_url('admin-ajax.php?action=' . $this->getAjaxAction())
+            \admin_url('admin-ajax.php?action='.$this->getAjaxAction())
         );
     }
 
@@ -71,23 +71,23 @@ class Module
     }
 
     /**
-     * render the view
+     * render the view.
      */
     public function render()
     {
-        if ( ! $this->doing_ajax) {
+        if (!$this->doing_ajax) {
             $this->set($GLOBALS['sloth::plugin']->getContext(), false);
         }
         $this->set('ajax_url', $this->getAjaxUrl());
         $this->beforeRender();
         $this->makeView();
-        $vars   = array_merge($GLOBALS['sloth::plugin']->getContext(), $this->viewVars);
+        $vars = array_merge($GLOBALS['sloth::plugin']->getContext(), $this->viewVars);
         $output = $this->view->with($vars)->render();
         if ($this->render) {
             if ($this->wrapInRow) {
                 $output = View::make('Layotter.row')->with([
                     'content' => $output,
-                    'options' => (array)$this->wrapInRow,
+                    'options' => (array) $this->wrapInRow,
                 ])->render();
             }
             echo $output;
@@ -102,12 +102,12 @@ class Module
         if (is_array($key)) {
             $override = is_bool($value) ? $value : true;
             foreach ($key as $k => $v) {
-                if ($override || ! $this->isSet($k)) {
+                if ($override || !$this->isSet($k)) {
                     $this->set($k, $v);
                 }
             }
         } else {
-            if ($override || ! $this->isSet($key)) {
+            if ($override || !$this->isSet($key)) {
                 $this->viewVars[$key] = $this->_prepareValue($value);
             }
         }
@@ -127,14 +127,14 @@ class Module
     {
         if (is_a($value, 'WP_Post')) {
             $model_name = $GLOBALS['sloth::plugin']->getPostTypeClass($value->post_type);
-            $post       = call_user_func([$model_name, 'find'], $value->ID);
-            $value      = $post;
+            $post = call_user_func([$model_name, 'find'], $value->ID);
+            $value = $post;
         }
 
         return $value;
     }
 
-    protected function beforeGetJSON()
+    protected function beforeGetJSON(array $payload)
     {
     }
 
@@ -155,15 +155,15 @@ class Module
     final private function getTemplate()
     {
         if (is_null($this->template)) {
-            $class          = get_class($this);
+            $class = get_class($this);
             $this->template = \Cake\Utility\Inflector::dasherize(preg_replace(
                 '/Module$/',
                 '',
-                substr(strrchr($class, "\\"), 1)
+                substr(strrchr($class, '\\'), 1)
             ));
         }
-        if ( ! strstr($this->template, '.')) {
-            $this->template = $this->viewPrefix . '.' . $this->template;
+        if (!strstr($this->template, '.')) {
+            $this->template = $this->viewPrefix.'.'.$this->template;
         }
         $this->template = str_replace('.', DS, ucfirst($this->template));
     }
